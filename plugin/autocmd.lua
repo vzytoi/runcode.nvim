7a7be57c1ce29672a2fe6c724d87c50c5f9e6898
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-12-03 11:40:37 +0100

    fix: close autocmd

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -2,7 +2,7 @@ nvim.create_autocmd("FileType", {
     pattern = "RunCode",
     callback = function()
         vim.g.nmap({ "<cr>", "q" }, function()
-            u.fun.close(vim.fn.bufnr())
+            vim.api.nvim_command("q")
         end, { buffer = 0 })
     end
 })
