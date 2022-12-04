e6693a6be94688b68b42262910a348d8cfa58498
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-12-04 11:39:57 +0100

    fix: undefined function call

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,8 +1,9 @@
 nvim.create_autocmd("FileType", {
     pattern = "RunCode",
     callback = function()
-        vim.g.nmap({ "<cr>", "q" }, function()
+        vim.keymap.set("n", "<cr>", function()
             vim.api.nvim_command("q")
         end, { buffer = 0 })
+
     end
 })
