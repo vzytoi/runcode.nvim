9a3822a61c962dd7597a5bfa69a323841e8c11a2
vzytoi <65293868+vzytoi@users.noreply.github.com>
2022-12-08 19:15:24 +0100

    feat: when closing runcode, place cursor correctly

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,9 +1,15 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "RunCode",
     callback = function()
-        vim.keymap.set("n", "<cr>", function()
-            vim.api.nvim_command("q")
-        end, { buffer = 0 })
 
+        vim.keymap.set("n", "<cr>", function()
+            local from = vim.api.nvim_buf_get_var(0, "From")
+
+            vim.api.nvim_command("q")
+
+            vim.api.nvim_set_current_win(
+                vim.fn.bufwinid(from)
+            )
+        end, { buffer = 0 })
     end
 })
